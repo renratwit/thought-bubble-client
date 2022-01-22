e9f683df2d757e1321cbf287db1c2a757ff63e02
renratwit <raymond.ren9@gmail.com>
2022-01-22 00:05:57 -0500

    removed unneeded functions

--- src/components/Thoughts.js
+++ src/components/Thoughts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Circle, MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 import {Icon} from 'leaflet';
+import { useEffect, useState } from 'react';
 //https://blog.logrocket.com/react-leaflet-tutorial/
 
 import { getThoughts } from '../api';
@@ -9,17 +10,6 @@ import Thought from './Thought';
 
 const Thoughts = ({messages}) => {
     console.log("THOUGHTS ", messages)
-
-    const handleButtonClick = async() => {
-        const thoughts = await getThoughts();
-        console.log(thoughts)
-        try {
-
-        }catch(e) {
-            console.error(e)
-        }
-    }
-    
     return (
         <div>
             {
@@ -27,7 +17,7 @@ const Thoughts = ({messages}) => {
                     <Thought thought={thought}/>
                 ))
             }
-            {/* <button onClick={handleButtonClick}>Refresh</button> */}
+
         </div>    
     )
 }
