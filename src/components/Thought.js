d486986ae818054596ac4b9f0c01bdf6b91efc7f
renratwit <raymond.ren9@gmail.com>
2022-02-23 12:16:39 -0500

    voting api endpoints

--- src/components/Thought.js
+++ src/components/Thought.js
@@ -1,15 +1,17 @@
 import {React, useContext} from 'react'
 import Map from './Map'
 import {useState, useEffect} from 'react'
-import { upVote, downVote } from '../api'
+import { upVote, downVote, likePost } from '../api'
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import styles from "../style/thought.css"
 
 import CommentForm from './CommentForm'
 import { UserContext } from './context/UserContext';
+import Favorite from '@mui/icons-material/Favorite';
 
 export default function Thought({thought}) {
     const [comments, setComments] = useState(thought.comments)
+    const [rating, setRating] = useState(thought.rating);
 
     const user = useContext(UserContext);
 
@@ -24,8 +26,12 @@ export default function Thought({thought}) {
 
     let isMyPost = thought.creator === user.email;
 
-    let handleLike = () => {
+    let handleLike = async () => {
         console.log("Clicked Heart")
+        let res = await likePost(thought, user)
+        console.log(res)
+        setRating(rating+1)
+
     }
 
     return (
@@ -38,10 +44,12 @@ export default function Thought({thought}) {
                 )
             }
 
-            {
+            {/* {
                 !isMyPost ? <FavoriteIcon className="like-button" onClick={handleLike}/> : <></>
+            } */
+                <Favorite className='like-button' onClick={handleLike}/>
             }
-            
+            {rating}
             <CommentForm thought={thought} handleCommentPost={handleCommentPost}/>
             <Map location={thought}/>
         </div>
